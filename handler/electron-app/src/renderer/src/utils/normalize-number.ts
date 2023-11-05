de9e8b71c2500fdc4cd2568983c267243c58b505
Tamas Halasi <trusted.tomato@gmail.com>
2023-11-05 18:43:26 +0100

    remove Electron app

--- handler/electron-app/src/renderer/src/utils/normalize-number.ts
+++
@@ -1,4 +0,0 @@
-/** Normalize a number so that it's value will be between 0 and 1. */
-export function normalizeNumber(value: number, min: number, max: number): number {
-  return (value - min) / (max - min)
-}
--- handler/electron-app/src/renderer/src/utils/process-axis-value.ts
+++
@@ -1,8 +0,0 @@
-const epsilon = 0.1
-
-export function processAxisValue(value: number): number {
-  if (Math.abs(value) < epsilon) {
-    return 0
-  }
-  return value
-}
